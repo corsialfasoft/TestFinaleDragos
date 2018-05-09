29aa909d53a013da74e28d8271389e630997549c
TandyD <brinzila30@gmail.com>
2018-05-09 15:21:52 +0200

    Esercizio2

---
+++ Esercizio2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio2 {
+    class Program {
+        static void Main(string[] args) {
+            string frase = "Cia. non so cosa scrivere. ma devo fare, ripetizioni, ripetizioni, ripetizioni, cosa scrivedere devo";
+            List<string> parole = new List<string>();
+            List<string> pNumeri = new List<string>();
+            string parola="";
+            frase = frase.ToLower();
+            for(int i = 0; i<frase.Length;i++){ 
+                if(frase[i]>='a' && frase[i]<='z'){
+                    parola = parola+frase[i];
+                }else{
+                    parole.Add(parola);
+                    parola ="";
+                }
+            }
+            foreach(string p in parole){ 
+                int x = 0;
+                foreach(string n in parole){ 
+                    if(p.Equals(n)){ 
+                        x++;    
+                    }    
+                }
+                if(!p.Equals("")){ 
+                    string daAdd = $"{p} --> {x}";
+                    if(!pNumeri.Contains(daAdd)){ 
+                        pNumeri.Add(daAdd);   
+                    }
+                }
+            }
+            foreach(string t in pNumeri){ 
+                Console.WriteLine(t);    
+            }
+        }
+    }
+}
--- TestFinaleDragos/Program.cs
+++ TestFinaleDragos/Program.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestFinaleDragos {
+namespace Esercizio1 {
     class Program {
-        static void Main(string[] args) {
-        }
+       /* static void Main(string[] args) {
+            int i = 0;
+            int stampa = 0;
+            while(i<15){ 
+                Console.WriteLine($"[{i+1}]  {stampa}");
+                stampa+=2;
+                i++;
+            }
+            Console.WriteLine("---------");
+            stampa-=3;
+            while(stampa>0){ 
+                Console.WriteLine(stampa);
+                stampa-=2;
+            }
+        }*/
     }
 }
